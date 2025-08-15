7b85efc818bf6dfa2971eab05deebe7eebc8d5f6
Chandan Kumar Sahu <chandansahu7980@gmail.com>
2025-08-15 11:38:11 +0530

    optimized code

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const taskInput = document.getElementById('taskInput');
 
 taskInput.focus();
 
+// Add event listener for the "Enter" key to add a task
+taskInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+});
+
 addTaskButton.addEventListener('click', function () {
+    addTask();
+});
+
+function addTask() {
     if (taskInput.value.trim() === '') {
         taskInput.value = '';
         taskInput.focus();
@@ -11,8 +22,13 @@ addTaskButton.addEventListener('click', function () {
     }
 
     const taskListElement = document.createElement('li');
-    taskListElement.innerHTML = `<div>${taskInput.value}</div><i class="delete_task fa-solid fa-circle-xmark"></i>`;
-    taskListElement.className = 'task-item';
+
+    taskListElement.innerHTML = `<div>
+                <input type="checkbox" id="task-${Date.now()}" />
+                <label for="task-${Date.now()}">${taskInput.value}</label>
+                </div>
+                <i class="delete_task fas fa-trash"></i>
+             `;
 
     const taskListContainer = document.getElementById('taskList');
     taskListContainer.appendChild(taskListElement);
@@ -20,10 +36,10 @@ addTaskButton.addEventListener('click', function () {
     // console.log("Task added: " + taskInput.value);
     taskInput.value = '';
     taskInput.focus();
-});
+}
 
 document.getElementById('taskList').addEventListener('click', function (event) {
-    if(event.target.classList.contains('delete_task')){
+    if (event.target.classList.contains('delete_task')) {
         const li = event.target.parentElement;
         li.remove();
     }
